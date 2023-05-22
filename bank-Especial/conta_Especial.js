36104216127bb2df7b22bbe02c796759d10bf230
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 13:36:59 -0300

    tarefa incompleta

---
+++ bank-Especial/conta_Especial.js
@@ -0,0 +1,80 @@
+class conta{
+    constructor(numero,cpf,saldo,ativo,limite){
+        this.numero = numero;
+        this.cpf = cpf;
+        this.saldo = saldo;
+        this.ativo = ativo;
+        this.limite = limite;
+    }
+
+    ativar(){
+        this.ativo = true;
+    }
+
+    credito(valor){
+        if(this.ativo){
+            if(valor<0){
+                console.log("impossivel realizar. valor negativo.")
+            }
+            else if(valor==0){				
+                console.log("Impossivel realizar, valor zerado...")				
+                }				
+            else {				
+            this.saldo += valor				
+            }				
+        }				
+            else {				
+            console.log("Conta inativa....")				
+            }				
+    }				
+
+
+    debito(){
+        if(this.ativo){
+            if(valor>0 && valor<this.saldo){
+                this.saldo -= valor
+            }
+            else if(valor>this.saldo){
+                if(valor-this.saldo>limite){
+                    console.log("Impossivel realizar. Valor supera o valor do saldo e o limite.")
+                }
+                else{
+                    this.limite -= this.saldo
+                    valor -= this.saldo
+                    this.saldo -= this.saldo
+                    this.limite -= valor
+                }
+
+            }
+            else{
+                console.log("impossivel realizar, valor invalido")
+            }
+        }
+        else{
+           console.log("conta inativa.") 
+        }
+    }
+}
+
+const leia = require("prompt-sync")()
+let numero = parseInt(leia("digite o numero da conta:"))
+let cpf = leia("digite o cpf da conta:")
+let c1 = new conta(numero,cpf,0,false)
+let op=""
+let valor = 0
+c1.ativar()
+for(let x=1; x<=10; x++){
+    console.log("movimento: "+x)
+    console.log("saldo atual conta: "+c1.saldo)
+    op=leia("digite D para debito ou C para credito: ")
+    if(op=="D"){
+        valor=parseInt (leia("digite o valor para debito: "))
+        c1.debito(valor)
+    }
+    else if(op=="C"){
+        valor=parseInt (leia("digite o valor para credito: "))
+        c1.credito(valor)
+    }
+}
+
+console.log("saldo final da conta: "+c1.saldo)
